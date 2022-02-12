2f58e7814659babaacdb1a174db8d6a37ba97881
Prabhat <bothraprabhat@gmail.com>
2022-02-12 18:27:38 +0530

    add api routes code

---
+++ pages/feedback/index.js
@@ -0,0 +1,45 @@
+import React, { Fragment, useState } from "react";
+import { buildFeedbackPath, extractFeedback } from "../api/feedback";
+
+const FeedbackPage = (props) => {
+  const [feedbackData, setFeedbackData] = useState();
+
+  const loadFeedbackHandler = (id) => {
+    fetch(`/api/${id}`)
+      .then((response /*  {}  CODE WORK WHY WHEN I REMOVE THIS FROM HERE*/) =>
+        response.json()
+      )
+      .then((data) => {
+        setFeedbackData(data.feedback);
+      });
+  };
+  return (
+    <Fragment>
+      {feedbackData && <p>{feedbackData.email}</p>}
+      <ul>
+        {props.feedbackItems.map((item) => (
+          <>
+            <li key={item.id}>
+              {item.text}
+              <button onClick={loadFeedbackHandler.bind(null, item.id)}>
+                Show Details
+              </button>
+            </li>
+          </>
+        ))}
+      </ul>
+    </Fragment>
+  );
+};
+
+export async function getStaticProps() {
+  const filePath = buildFeedbackPath();
+  const data = extractFeedback(filePath);
+
+  return {
+    props: {
+      feedbackItems: data,
+    },
+  };
+}
+export default FeedbackPage;
